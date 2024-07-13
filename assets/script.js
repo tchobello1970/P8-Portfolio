8e68842afb4ae956495149dc18925d94826984be
Yannick Priol <yannickpriol@orange.fr>
2024-07-13 16:12:18 +0200

    about section

--- assets/script.js
+++ assets/script.js
@@ -11,5 +11,31 @@ document.addEventListener('DOMContentLoaded', function() {
         sunIcon.classList.toggle('hidden');
     });
 
+    const hobbiesGrid = document.getElementById('hobbiesGrid');
+    const hobbyItems = hobbiesGrid.querySelectorAll('.hobby-item');
+
+    hobbyItems.forEach(item => {
+        const frontImage = item.getAttribute('data-front');
+        const backImage = item.getAttribute('data-back');
+
+        item.innerHTML = `
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="${frontImage}" alt="">
+                </div>
+                <div class="flip-card-back">
+                    <img src="${backImage}" alt="">
+                </div>
+            </div>
+        `;
+
+        if (window.innerWidth <= 768) {
+            item.addEventListener('click', () => {
+                console.log('item clicked');
+                item.classList.toggle('flipped');
+            });
+        }
+    });
+
 } );
 
