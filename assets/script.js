918be12ddfb31d4b16d4b8ed905000305693b90b
Yannick Priol <yannickpriol@orange.fr>
2024-07-24 14:26:18 +0200

    img webp

--- assets/script.js
+++ assets/script.js
@@ -1,149 +1,109 @@
-
-document.addEventListener('scroll', function() {
+document.addEventListener('DOMContentLoaded', () => {
+    // Variables globales pour les éléments DOM
     const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('.nav-links a');
-    let currentSection = '';
-
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        if (scrollY >= sectionTop - 60) { // 60 is the offset for the fixed navbar height
-            currentSection = section.getAttribute('id');
-        }
-    });
-
-    navLinks.forEach(link => {
-        link.classList.remove('active');
-        if (link.getAttribute('href').substring(1) === currentSection) {
-            link.classList.add('active');
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
     const toggleDarkModeBtn = document.getElementById('toggle-dark-mode');
-    const toggleLanguageBtn = document.getElementById('toggle-language');
-
     const moonIcon = document.getElementById('moon-icon');
     const sunIcon = document.getElementById('sun-icon');
+    const menuBtn = document.querySelector('.menu-btn');
+    const navLinksContainer = document.querySelector('.nav-links');
+    const hobbiesGrid = document.getElementById('hobbiesGrid');
+    const clipboardIcon = document.getElementById('clipboard-icon');
+    const mailText = document.getElementById('email-text');
+    const mailCopyDiv = document.querySelector('.mail-copy');
 
-    toggleDarkModeBtn.addEventListener('click', function() {
+    // Fonction pour mettre à jour les liens de navigation en fonction du défilement
+    const updateNavLinks = () => {
+        let currentSection = '';
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            if (scrollY >= sectionTop - 60) { // 60 est le décalage pour la hauteur de la barre de navigation fixe
+                currentSection = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.toggle('active', link.getAttribute('href').substring(1) === currentSection);
+        });
+    };
+
+    // Fonction pour gérer le mode sombre
+    const toggleDarkMode = () => {
         document.body.classList.toggle('dark-mode');
         moonIcon.classList.toggle('hidden');
         sunIcon.classList.toggle('hidden');
-    });
+    };
 
-    const menuBtn = document.querySelector('.menu-btn');
-    const navLinks = document.querySelector('.nav-links');
+    // Fonction pour gérer le menu
+    const toggleMenu = () => {
+        navLinksContainer.classList.toggle('show');
+    };
 
-    menuBtn.addEventListener('click', () => {
-        navLinks.classList.toggle('show');
-    });
+    // Fonction pour gérer les cartes de loisirs
+    const initializeHobbies = () => {
+        const hobbyItems = hobbiesGrid.querySelectorAll('.hobby-item');
+        hobbyItems.forEach(item => {
+            const frontImage = item.getAttribute('data-front');
+            const backImage = item.getAttribute('data-back');
 
-
-    const hobbiesGrid = document.getElementById('hobbiesGrid');
-    const hobbyItems = hobbiesGrid.querySelectorAll('.hobby-item');
-
-    hobbyItems.forEach(item => {
-        const frontImage = item.getAttribute('data-front');
-        const backImage = item.getAttribute('data-back');
-        
-        item.innerHTML = `
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
-                    <img src="${frontImage}" alt="">
+            item.innerHTML = `
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img src="${frontImage}" alt="">
+                    </div>
+                    <div class="flip-card-back">
+                        <img src="${backImage}" alt="">
+                    </div>
                 </div>
-                <div class="flip-card-back">
-                    <img src="${backImage}" alt="">
-                </div>
-            </div>
-        `;
+            `;
 
-        if (window.innerWidth <= 768) {
-            item.addEventListener('click', () => {
-                item.classList.toggle('flipped');
-            });
-        }
-    });
+            if (window.innerWidth <= 768) {
+                item.addEventListener('click', () => {
+                    item.classList.toggle('flipped');
+                });
+            }
+        });
+    };
 
-
-
-    var clipboardIcon = document.getElementById('clipboard-icon');
-    var mailText = document.getElementById('email-text');
-    var mailCopyDiv = document.querySelector('.mail-copy');
-
-        document.getElementById('email-text').addEventListener('click', function() {
-        copyToClipboard(mailText.innerText); 
-    });
-
-        clipboardIcon.addEventListener('click', function() {
-        copyToClipboard(mailText.innerText);
-    });
-
-    function copyToClipboard(text) {
+    // Fonction pour copier du texte dans le presse-papiers
+    const copyToClipboard = text => {
         navigator.clipboard.writeText(text)
-            .then(function() {
-                clipboardIcon.classList.remove('fa-clipboard');
-                clipboardIcon.classList.add('fa-check-circle');
+            .then(() => {
+                clipboardIcon.classList.replace('fa-clipboard', 'fa-check-circle');
                 clipboardIcon.style.color = 'green';
 
-                var tempMessage = document.createElement('p');
+                const tempMessage = document.createElement('p');
                 tempMessage.textContent = 'Ajouté au presse-papiers';
                 tempMessage.style.color = 'green';
                 tempMessage.style.marginLeft = '10px';
 
                 clipboardIcon.insertAdjacentElement('afterend', tempMessage);
 
-                setTimeout(function() {
-                    clipboardIcon.classList.remove('fa-check-circle');
-                    clipboardIcon.classList.remove('fa-regular');
-                    clipboardIcon.classList.add('fa-clipboard');
-                    clipboardIcon.classList.add('fa-solid');
+                setTimeout(() => {
+                    clipboardIcon.classList.replace('fa-check-circle', 'fa-clipboard');
                     clipboardIcon.style.color = 'var(--text-color)';
-
                     mailCopyDiv.removeChild(tempMessage);
                 }, 2000);
             })
-            .catch(function(err) {
+            .catch(err => {
                 console.error('Erreur lors de la copie dans le presse-papiers : ', err);
             });
-    }
+    };
 
+    // Fonction pour ouvrir une modale
+    const openModal = modalId => {
+        const modal = document.getElementById(modalId);
+        if (modal) modal.style.display = 'block';
+    };
 
-    function openModal(modalId) {
-        document.getElementById(modalId).style.display = "block";
-    }
-    
-    function closeModal(modalId) {
-        document.getElementById(modalId).style.display = "none";
-    }
-    
-    document.querySelectorAll('.fa-question-circle').forEach(function(element) {
-        element.onclick = function() {
-            var modalId = this.getAttribute('data-modal');
-            openModal(modalId);
-        }
-    });
-    
-    document.querySelectorAll('.close').forEach(function(element) {
-        element.onclick = function() {
-            var modalId = this.getAttribute('data-modal');
-            closeModal(modalId);
-        }
-    });
-    
-    window.onclick = function(event) {
-        if (event.target.classList.contains('modal')) {
-            closeModal(event.target.id);
-        }
-    }
+    // Fonction pour fermer une modale
+    const closeModal = modalId => {
+        const modal = document.getElementById(modalId);
+        if (modal) modal.style.display = 'none';
+    };
 
-    document.querySelector('form').addEventListener('submit', function(event) {
-        if (!validateForm()) {
-            event.preventDefault();
-        }
-    });
-
-    function validateForm() {
+    // Fonction de validation de formulaire
+    const validateForm = () => {
         const email = document.getElementById('email').value;
         const message = document.getElementById('message').value.trim();
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -159,6 +119,33 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         return true;
-    }
-});
+    };
 
+    // Ajouter les écouteurs d'événements
+    document.addEventListener('scroll', updateNavLinks);
+    toggleDarkModeBtn && toggleDarkModeBtn.addEventListener('click', toggleDarkMode);
+    menuBtn && menuBtn.addEventListener('click', toggleMenu);
+    hobbiesGrid && initializeHobbies();
+    clipboardIcon && clipboardIcon.addEventListener('click', () => copyToClipboard(mailText.innerText));
+    mailText && mailText.addEventListener('click', () => copyToClipboard(mailText.innerText));
+
+    document.querySelectorAll('.fa-question-circle').forEach(element => {
+        element.addEventListener('click', () => openModal(element.getAttribute('data-modal')));
+    });
+
+    document.querySelectorAll('.close').forEach(element => {
+        element.addEventListener('click', () => closeModal(element.getAttribute('data-modal')));
+    });
+
+    window.addEventListener('click', event => {
+        if (event.target.classList.contains('modal')) {
+            closeModal(event.target.id);
+        }
+    });
+
+    document.querySelector('form')?.addEventListener('submit', event => {
+        if (!validateForm()) {
+            event.preventDefault();
+        }
+    });
+});
